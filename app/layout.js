646c48255299b396209001331986aa84df88857d
shruti0101 <shrutiguptacu@gmail.com>
2025-07-26 11:30:01 +0530

    final commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-// app/layout.jsx or layout.tsx
-'use client';
+
 
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -7,22 +6,28 @@ import Navbar from '@/components/Navbar/Navbar';
 import Footer from "@/components/Footer/Footer";
 import { Toaster } from 'react-hot-toast';
 
-
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
 });
 
+// ✅ Add this metadata object
+export const metadata = {
+  title: 'Chintan Agrawal & CO.',
+  description: '',
+  icons: {
+    icon: '/logo.png',
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
- 
-          <Toaster position="top-right" reverseOrder={false} />
-          <Navbar />
-          {children}
-          <Footer />
-     
+        <Toaster position="top-right" reverseOrder={false} />
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );

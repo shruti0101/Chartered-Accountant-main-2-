71b77421dc42c8dbefafcbbf34a76603a94de852
shruti0101 <shrutiguptacu@gmail.com>
2025-08-08 10:26:53 +0530

    commit

--- Database/Db.js
+++ Database/Db.js
@@ -7,8 +7,8 @@ export const connect = async () => {
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+   
+ 
     });
 
     isConnected = conn.connections[0].readyState === 1;
